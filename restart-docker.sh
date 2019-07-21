e6362e4a26b19af746aa496d4b4f3b5bb102076d
Tom Broughton <hello@tombroughton.me>
2019-07-21 12:00:15 +0100

    whilst developing and using restart-docker composer install will pull from cache

--- restart-docker.sh
+++ restart-docker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
+# I just use this to help me quickly restart my docker
+# whilst I am developing - we should look at better ways
 docker-compose down
-rm -rf build/ .composer/ composer.lock .drush/ mysql/ vendor/
-docker-compose up -d
+rm -rf build/ composer.lock .drush/ mysql/ vendor/
+docker-compose up
 docker logs -f backdrop
