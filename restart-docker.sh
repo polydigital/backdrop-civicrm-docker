e8698cd3f0ef5a5f431ac2b3078747883d52ba87
Tom Broughton <hello@tombroughton.me>
2019-08-23 13:44:27 +0100

    simplified automatic writing of hostipaddress on restart helper

--- restart-docker.sh
+++ restart-docker.sh
@@ -7,7 +7,7 @@ rm -rf build/ composer.lock mysql/ vendor/ .drush
 #update host IP in environment vars to write to docker
 #container hosts files https://github.com/polydigital/backdrop-civicrm-docker/issues/15
 source .env
-newhostip=`ip -4 addr show scope global dev docker0 | grep inet | awk '{print \$2}' | cut -d / -f 1`
+newhostip=`ip -4 addr show docker0 | grep -Po 'inet \K[\d.]+'`
 sed -i "s@\b${HOST_MACHINE_IP}\b@${newhostip}@g" .env
 
 docker-compose up
